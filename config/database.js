9228ae7b1f0de8c1be8244f2319bacb9fd0816ba
KyeongHoon Kim <71899948+khoon-git@users.noreply.github.com>
2022-03-28 11:40:27 +0900

    Update database.js

--- config/database.js
+++ config/database.js
@@ -7,11 +7,11 @@ module.exports = function () {
     return {
         init: function () {
             return mysql.createConnection({
-                host : "34.64.225.42", //서버 로컬 IP
-                port : 9876,
-                user : "root", //계정 아이디
-                password : "1234", //계정 비밀번호
-                database : "WHO_WEB" //접속할 DB
+                host : "", //서버 로컬 IP
+                port : "" ,
+                user : "", //계정 아이디
+                password : "", //계정 비밀번호
+                database : "" //접속할 DB
             })
         },
 
